38ad9f1f6935d4382dea75bd88c6614f3dabfc2e
daveclinton <clintondavid46@gmail.com>
2025-05-16 14:37:07 +0300

    feat: add velt api key

--- lib/context/velt-wrapper.tsx
+++ lib/context/velt-wrapper.tsx
@@ -9,7 +9,7 @@ import {
 export function VeltWrapper({ children }: { children: ReactNode }) {
   return (
     <VeltProvider apiKey={process.env.NEXT_PUBLIC_VELT_KEY || ""}>
-      <VeltComments dialogShadowDom={false} darkMode={true} />
+      <VeltComments textMode={false} darkMode={true} />
       <VeltCommentsSidebar />
       {children}
     </VeltProvider>
