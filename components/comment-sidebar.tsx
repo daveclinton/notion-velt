ba05fbe840c17fa1f69553d80ce03111d1a9b697
daveclinton <clintondavid46@gmail.com>
2025-05-19 21:51:39 +0300

    feat: add cover image

--- components/comment-sidebar.tsx
+++ components/comment-sidebar.tsx
@@ -11,7 +11,17 @@ import {
   MoreHorizontal,
   MessageSquareText,
   ChevronsLeft,
+  Sun,
+  Moon,
 } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
 
 interface Comment {
   id: number;
@@ -25,6 +35,8 @@ interface CommentSidebarProps {
 }
 
 export const CommentSidebar: FC<CommentSidebarProps> = ({ documentId }) => {
+  const { setTheme } = useTheme();
+
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [notifications, setNotifications] = useState<
     "all" | "mentions" | "none"
@@ -81,9 +93,26 @@ export const CommentSidebar: FC<CommentSidebarProps> = ({ documentId }) => {
         <button className="p-1 hover:bg-[color:var(--color-sidebar-accent)] cursor-pointer rounded">
           <Star size={20} />
         </button>
-        <button className="p-1 hover:bg-[color:var(--color-sidebar-accent)] cursor-pointer rounded">
-          <MoreHorizontal size={20} />
-        </button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
